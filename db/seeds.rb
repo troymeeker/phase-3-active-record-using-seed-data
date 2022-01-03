66e432cda818e80973a587723f1fca87440633a4
troymeeker <troymeeker1@gmail.com>
2022-01-03 12:40:56 -0800

    commit tree ed3156abd0e25ca2d54fbd6848b75ccc781b5d2d

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 # Add a console message so we can see output when the seed file runs
+50.times do 
+    Game.create(
+        title:Faker::Game.title, 
+        genre:Faker::Game.genre, 
+        platform: Faker::Game.platform,
+        price: rand(0..60)
+    )
+end
+
+puts "Done Seeding!"
